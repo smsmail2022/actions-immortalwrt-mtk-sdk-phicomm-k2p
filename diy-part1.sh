b997098968fd6b0e92d02a20217a2e094a78ef18
smsmail2022 <123883726+smsmail2022@users.noreply.github.com>
2023-02-02 20:03:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,9 +17,10 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git luci-app-omcproxy https://github.com/mumuou/luci-app-omcproxy' >>feeds.conf.default
-echo 'src-git luci-app-omcproxy https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-omcproxy' >>feeds.conf.default
+# echo 'src-git luci-app-omcproxy https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-omcproxy' >>feeds.conf.default
 echo 'src-git luci-app-aliyundrive-webdav https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-aliyundrive-webdav' >>feeds.conf.default
 # svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-omcproxy package/luci-app-omcproxy
 # svn export https://github.com/riverscn/luci-app-omcproxy package/luci-app-omcproxy
 # svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-aliyundrive-webdav package/luci-app-aliyundrive-webdav
 # echo 'src-git luci https://github.com/immortalwrt/luci' >>feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/riverscn/luci-app-omcproxy:18.06' feeds.conf.default
