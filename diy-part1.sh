f0da59f46f982d499a667acb2b6d4539e4823b03
smsmail2022 <123883726+smsmail2022@users.noreply.github.com>
2023-02-09 20:12:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,14 +9,14 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
 # Uncomment a feed source
-sed -i 's/^#\(.*luci-app-omcproxy\)/\1/' feeds.conf.default
-sed -i '$a src-git luci-app-omcproxy https://github.com/smsmail2022/luci-app-omcproxy' feeds.conf.default
-
+# sed -i 's/^#\(.*luci-app-omcproxy\)/\1/' feeds.conf.default
+# sed -i '$a src-git luci-app-omcproxy https://github.com/smsmail2022/luci-app-omcproxy' feeds.conf.default
+#
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git luci-app-omcproxy https://github.com/mumuou/luci-app-omcproxy' >>feeds.conf.default
 # echo 'src-git luci-app-omcproxy https://github.com/smsmail2022/luci-app-omcproxy' >>feeds.conf.default
 # echo 'src-git luci-app-aliyundrive-webdav https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-aliyundrive-webdav' >>feeds.conf.default
+git clone https://github.com/smsmail2022/luci-app-omcproxy.git package/luci-app-omcproxy
