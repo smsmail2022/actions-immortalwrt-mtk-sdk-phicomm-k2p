12fa89f1a6df27ae156d6f26af5f0c3e6f1558f6
smsmail2022 <123883726+smsmail2022@users.noreply.github.com>
2023-02-11 14:55:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# echo 'src-git luci-app-omcproxy https://github.com/mumuou/luci-app-omcproxy' >>feeds.conf.default
-# echo 'src-git luci-app-omcproxy https://github.com/smsmail2022/luci-app-omcproxy' >>feeds.conf.default
-# echo 'src-git luci-app-aliyundrive-webdav https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-aliyundrive-webdav' >>feeds.conf.default
-# git clone https://github.com/smsmail2022/luci-app-omcproxy.git package/luci-app-omcproxy
+
+#echo '添加omcproxy软件源'
+#git clone https://github.com/smsmail2022/luci-app-omcproxy package/luci-app-omcproxy
+git clone -b 18.06 https://github.com/riverscn/luci-app-omcproxy.git package/luci-app-omcproxy
